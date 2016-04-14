649d88412c93ce97edece3cb463491476d30c55a
Fathima Sifa Syed Musthafa <fathima.sifa.syed.musthafa@macys.com>
2016-04-14 11:38:48 -0700

    PracticeMergeConflits.java committed

---
+++ PracticeMergeConfilcts.java
@@ -0,0 +1,19 @@
+public class PracticeMergeConflictis{
+
+	public void fnOne(){
+
+		System.out.println("Welcome");
+
+	}
+
+	public static void main (String[] args)
+
+	{
+
+		PracticeMergeConflictis mc = new PracticeMergeConflictis();
+
+		mc.fnOne();
+
+	}
+
+}
