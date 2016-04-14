838605559c3ef502981e023d1a0231143d8384b7
Fathima Sifa Syed Musthafa <fathima.sifa.syed.musthafa@macys.com>
2016-04-14 14:46:12 -0700

    Added Header

--- PracticeMergeConfilcts.java
+++ PracticeMergeConfilcts.java
@@ -6,7 +6,7 @@ public class PracticeMergeConflictis{
 
 	}
 	public void upcomingEvents(){
-
+	System.out.println("Upcoming Events Header");
 	System.out.println("Party at SFO California Pizza");
 
 	}
